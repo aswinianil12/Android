b517022f9d124239827e486f45c22f52205f3bad
aswinianil12 <aswinianil918@gmail.com>
2025-09-19 14:43:24 +0530

    Update multiple_activity1.java

--- multiple_activity1.java
+++ multiple_activity1.java
@@ -8,14 +8,14 @@ import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity2 extends AppCompatActivity {
-    Button b2;
+public class MainActivity extends AppCompatActivity {
+    Button b1;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
-        b2 = findViewById(R.id.page2);
-        b2.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_main);
+        b1= findViewById(R.id.page1);
+        b1.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent i = new Intent(MainActivity2.this, MainActivity.class);
                 startActivity(i);
